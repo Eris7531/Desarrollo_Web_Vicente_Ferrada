68de4ab0d6243f3f49f43f87aaa67fb302f4b6f9
Eris7531 <vicente.ferrada@ing.uchile.cl>
2026-05-13 00:00:29 -0400

    Removed files from old HW and others not related to the final product

--- T2Test/flask_app/database/create_user.sql
+++
@@ -1,4 +0,0 @@
--- Usuario indicado en el enunciado (host local, MySQL/MariaDB)
-CREATE USER IF NOT EXISTS 'cc5002'@'localhost' IDENTIFIED BY 'programacionweb';
-GRANT ALL PRIVILEGES ON tarea2.* TO 'cc5002'@'localhost';
-FLUSH PRIVILEGES;
