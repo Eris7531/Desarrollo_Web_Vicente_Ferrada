79bbb6c0c699089aa17b6ba544fcfa86bbe68d15
Eris7531 <vicente.ferrada@ing.uchile.cl>
2026-06-27 23:54:55 -0400

    inicializado spring usando instrucciones de aux 9

---
+++ Tarea 4/spring_app/tarea4spring/src/main/java/com/tarea4spring/tarea4spring/Tarea4springApplication.java
@@ -0,0 +1,13 @@
+package com.tarea4spring.tarea4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea4springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea4springApplication.class, args);
+	}
+
+}
---
+++ Tarea 4/spring_app/tarea4spring/src/test/java/com/tarea4spring/tarea4spring/Tarea4springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea4spring.tarea4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea4springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
